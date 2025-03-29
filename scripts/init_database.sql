f3392927d4201ece228c7dad52b647242d7915f6
Heiner <80844464+Crx-cmd@users.noreply.github.com>
2025-03-29 16:32:43 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,4 +1,18 @@
+/*
+==============================================
+CREATE DATABASE AND SCHEMA
+==============================================
+
+This scripts creates a new database 'DataWarehouse'
+
+
+*/
+
+
+
 CREATE DATABASE Warehouse;
+
+-- CREATE SCHMAS (MEDALLIONS)
 CREATE SCHEMA bronze;
 CREATE SCHEMA silber;
 CREATE SCHEMA gold;
